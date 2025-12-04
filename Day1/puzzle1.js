e5af6fc9f1ef8abe9a11e8e235eb9e043ce4e6c7
AlexLouk <AlexLouk@icloud.com>
2025-12-04 21:57:06 +0100

    Day 1 puzzle 1

---
+++ Day1/puzzle1.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs');
+const { start } = require('node:repl');
+
+function readAndFormatData() {
+    return fs.readFileSync('./input.txt', 'utf-8')
+        .trim()
+        .split('\r\n')
+        .map(line => {
+            const direction = line[0];
+            const steps = parseInt(line.slice(1));
+            return { direction, steps };
+        })
+}
+
+function puzzle1() {
+    const data = readAndFormatData();
+    let startingPoint = 50;
+    let zeroes = 0;
+
+    data.forEach(line => {
+        let oldPos = startingPoint;
+        zeroes += Math.floor(Math.abs(line.steps) / 100);
+
+        if (line.direction === "L") {
+            startingPoint -= line.steps;
+            if (oldPos >= 0 && startingPoint < 0){
+                zeroes++;
+            }
+        } else {
+            startingPoint += line.steps;
+            if (oldPos <= 99 && startingPoint > 99){
+                zeroes++;
+            }
+        }
+
+        startingPoint = ((startingPoint % 100) + 100) % 100;
+    })
+
+    console.log(`The amount of Zeroes hit is: ${zeroes}`);
+}
+
+puzzle1();
